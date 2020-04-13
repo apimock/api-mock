9b4ad56853257353932aed2bbeb1240f7c15b4c4
bobby169 <115393304@qq.com>
2020-04-13 19:57:20 +0800

    feat: add koa-validate

--- server/split.js
+++ server/split.js
@@ -1,14 +1,16 @@
 require('@babel/register')
 const moduleAlias = require('module-alias')
 const Koa = require('koa')
-const bodyParser = require('koa-bodyparser')
+const koaBody = require('koa-body')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
+const validate = require('koa-validate')
 const config = require('../nuxt.config.js')
 const app = new Koa()
 config.dev = app.env !== 'production'
 moduleAlias.addAliases(require('../alias').resolve.alias)
-app.use(bodyParser())
+validate(app)
+app.use(koaBody({ multipart: true }))
 
 const routes = require('./routes')
 routes(app)
