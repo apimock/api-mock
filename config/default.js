06be2fd1e5f05c73c91853bef5cee1aff08abb81
bobby169 <115393304@qq.com>
2020-05-06 14:15:53 +0800

    fix: config repeat bug

--- config/default.js
+++ config/default.js
@@ -4,41 +4,8 @@ module.exports = {
   pageSize: 30,
   mysql: {
     database: 'apimock',
-    user: 'test',
-    password: '123456789',
-    option: {
-      host: '127.0.0.1',
-      port: 3306,
-      dialect: 'mysql',
-      timezone: '+08:00', // 时区 香港 北京
-      // mysql连接池配置
-      pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-      },
-      define: {
-        timestamps: false // 不自动添加 updateAt 和 createdAt
-      },
-      logging (sql, { bind }) {
-        const res = bind ? sql + bind : sql
-        console.info(res)
-      }
-    }
-  },
-  jwt: {
-    'expire': '14 days',
-    'secret': 'apimock-secret'
-  }
-}
-module.exports = {
-  port: 8888,
-  host: '127.0.0.1',
-  pageSize: 30,
-  mysql: {
-    database: 'apimock',
     user: 'root',
-    password: '123456789',
+    password: 'root',
     option: {
       host: '127.0.0.1',
       port: 3306,
--- config/production.js
+++ config/production.js
@@ -31,36 +31,3 @@ module.exports = {
     'secret': 'apimock-secret'
   }
 }
-module.exports = {
-  port: 8888,
-  host: '127.0.0.1',
-  pageSize: 30,
-  mysql: {
-    database: 'apimock',
-    user: 'root',
-    password: 'root',
-    option: {
-      host: '127.0.0.1',
-      port: 3306,
-      dialect: 'mysql',
-      timezone: '+08:00', // 时区 香港 北京
-      // mysql连接池配置
-      pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-      },
-      define: {
-        timestamps: false // 不自动添加 updateAt 和 createdAt
-      },
-      logging (sql, { bind }) {
-        const res = bind ? sql + bind : sql
-        console.info(res)
-      }
-    }
-  },
-  jwt: {
-    'expire': '14 days',
-    'secret': 'apimock-secret'
-  }
-}
