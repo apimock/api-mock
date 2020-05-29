7b91f52d7c084ba16d28de2ebb1b34875c611c8e
bobby169 <115393304@qq.com>
2020-05-29 20:47:42 +0800

    fix: do not modify api raw option

--- src/utils/wraperApi.js
+++ src/utils/wraperApi.js
@@ -10,10 +10,13 @@ export default function wraperApi (api) {
   const obj = Object.create(null)
   for (const [name, option] of Object.entries(api)) {
     obj[name] = (parameter, urlObj) => {
+      if (!option._raw) {
+        option._raw = Object.freeze(Object.assign(Object.create(null), option))
+      }
       if (urlObj) {
-        option.url = sub(option.url, urlObj)
-      } else {
-        option.url = sub(option.url, parameter)
+        option.url = sub(option._raw.url, urlObj)
+      } else if (parameter) {
+        option.url = sub(option._raw.url, parameter)
       }
       if (option.method === 'get') {
         option.params = parameter
